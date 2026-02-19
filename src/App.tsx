cf665abcbec7824aec9c2565059119a1a64be632
Lucashuriken <lucas.hsousa@al.infnet.edu.br>
2026-02-19 16:16:39 -0300

    alterações minimas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import EventosPage from "./pages/EventosPage";
 import TemaPage from "./pages/TemaPage";
-import CredenciaisPage from "./pages/CredenciaisPage";      
-// troque aqui para a page desejada
+import CredenciaisPage from "./pages/CredenciaisPage";
+import FormularioPage from "./pages/FormularioPage";
+import MouseTrackerPage from "./pages/MouseTrackerPage";
+import KeyboardPage from "./pages/KeyboardPage";
+import VisibilidadePage from "./pages/VisibilidadePage"; 
+
+// Usar o return em cada pagina importada, para visualização das tarefas exigidas no TP !
+
 
 function App() {
   return <CredenciaisPage />;
